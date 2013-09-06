f461627019adb5451a6afc94d449ff6fa7866806
Victor Manuel Cervantes <vicmns@gmail.com>
2013-09-06 15:08:24 -0500

    Added license to source

--- mp4parserAppTest/src/com/vicmns/mp4parserapptest/MainActivity.java
+++ mp4parserAppTest/src/com/vicmns/mp4parserapptest/MainActivity.java
@@ -1,3 +1,18 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.vicmns.mp4parserapptest;
 
 import java.io.File;
@@ -104,7 +119,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		
 		switch(view.getId()){
 			case R.id.mergeBttn:
-				//TODO: Call merge asynctask
 				mergeVideos = new MergeVideos(workingPath, videosToMerge).execute();
 			break;
 			case R.id.trimmBttn:
